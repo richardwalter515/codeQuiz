f4f71f9dd668ef5751d979bf37568dc4bbd79319
Richard Walter <richardwalter515@gmail.com>
2020-09-25 10:42:59 -0400

    fixed timer

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-var time = 60;
-var startButton = document.getElementById(startbutton);
-console.log('startButton:', startButton)
+var timeLeft = 60;
+var startButton = document.querySelector("#startButton");
+var timeEl = document.querySelector('#timer');
+var questions = [
 
-var timeEl = document.getElementsByClassName('.timer')
+];
 
-startButton.addEventListener("click", function() {
-    time--;
-    counter.textContent = count;
+//when start button is clicked, begin timer
+startButton.addEventListener('click', function(setTime) {
+  var timerInterval = setInterval(function() {
+    timeLeft--;
+    timeEl.textContent = timeLeft;
+    console.log('timeEl:', timeEl)
   
-  });
+    if(timeLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+})
 
+function askQuestion() {
 
-  //if rightAnswer is clicked, score ++ and nextquestion, else nextquestion
+}
+
+//while time is > 0, ask questions
+while (timeLeft > 0) {
+  askQuestion();
+}
+//if rightAnswer is clicked, score ++ and nextquestion, else time -10 and nextquestion
+//store score in highscores local storage
+
+//questions - should the questions be in an Array?
+//How many questions should there be?
+//how to cycle through questions?
